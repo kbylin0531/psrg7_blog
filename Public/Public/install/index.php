dd2b86dd85cd3be228e834d0954773a7da7d8089
linzhv <784855684@qq.com>
2016-11-09 15:10:40 +0800

    fix done

--- Public/Public/install/index.php
+++ Public/Public/install/index.php
@@ -3,56 +3,60 @@
  * 安装向导
  */
 header('Content-type:text/html;charset=utf-8');
+
+define('SR_INSTALL_PATH', __DIR__ . '/');
 // 检测是否安装过
-if (file_exists('./install.lock')) {
-    echo '你已经安装过该系统，重新安装需要先删除./Public/install/install.lock 文件';
-    die;
+if (file_exists(SR_INSTALL_PATH . 'install.lock')) {
+    die('你已经安装过该系统，重新安装需要先删除./Public/install/install.lock 文件');
 }
-// 同意协议页面
-if(@!isset($_GET['c']) || @$_GET['c']=='agreement'){
-    require './agreement.html';
+
+if (!isset($_GET['c'])) {
+    $_GET['c'] = 'agreement';
 }
-// 检测环境页面
-if(@$_GET['c']=='test'){
-    require './test.html';
-}
-// 创建数据库页面
-if(@$_GET['c']=='create'){
-    require './create.html';
-}
-// 安装成功页面
-if(@$_GET['c']=='success'){
-    // 判断是否为post
-    if($_SERVER['REQUEST_METHOD']=='POST'){
-        $data=$_POST;
-        // 连接数据库
-        $link=@new mysqli("{$data['DB_HOST']}:{$data['DB_PORT']}",$data['DB_USER'],$data['DB_PWD']);
-        // 获取错误信息
-        $error=$link->connect_error;
-        if (!is_null($error)) {
-            // 转义防止和alert中的引号冲突
-            $error=addslashes($error);
-            die("<script>alert('数据库链接失败:$error');history.go(-1)</script>");
-        }
-        // 设置字符集
-        $link->query("SET NAMES 'utf8'");
-        $link->server_info>5.0 or die("<script>alert('请将您的mysql升级到5.0以上');history.go(-1)</script>");
-        // 创建数据库并选中
-        if(!$link->select_db($data['DB_NAME'])){
-            $create_sql='CREATE DATABASE IF NOT EXISTS '.$data['DB_NAME'].' DEFAULT CHARACTER SET utf8;';
-            $link->query($create_sql) or die('创建数据库失败');
-            $link->select_db($data['DB_NAME']);
-        }
-        // 导入sql数据并创建表
-        $bjyblog_str=file_get_contents('./bjyblog.sql');
-        $sql_array=preg_split("/;[\r\n]+/", str_replace('bjy_',$data['DB_PREFIX'],$bjyblog_str));
-        foreach ($sql_array as $k => $v) {
-            if (!empty($v)) {
-                $link->query($v);
+switch ($_GET['c']) {
+    // 同意协议页面
+    case 'agreement':
+        require SR_INSTALL_PATH . '/agreement.php';
+        break;
+    // 检测环境页面
+    case 'test':
+        require SR_INSTALL_PATH . '/test.php';
+        break;
+    // 创建数据库页面
+    case 'create':
+        require SR_INSTALL_PATH . '/create.php';
+        break;
+    case 'success':
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $data = $_POST;
+            // 连接数据库
+            $link = @new mysqli("{$data['DB_HOST']}:{$data['DB_PORT']}", $data['DB_USER'], $data['DB_PWD']);
+            // 获取错误信息
+            $error = $link->connect_error;
+            if (!is_null($error)) {
+                // 转义防止和alert中的引号冲突
+                $error = addslashes($error);
+                die("<script>alert('数据库链接失败:$error');history.go(-1)</script>");
             }
-        }
-        $link->close();
-        $db_str=<<<php
+            // 设置字符集
+            $link->query("SET NAMES 'utf8'");
+            $link->server_info > 5.0 or die("<script>alert('请将您的mysql升级到5.0以上');history.go(-1)</script>");
+            // 创建数据库并选中
+            if (!$link->select_db($data['DB_NAME'])) {
+                $create_sql = 'CREATE DATABASE IF NOT EXISTS ' . $data['DB_NAME'] . ' DEFAULT CHARACTER SET utf8;';
+                $link->query($create_sql) or die('创建数据库失败');
+                $link->select_db($data['DB_NAME']);
+            }
+            // 导入sql数据并创建表
+            $bjyblog_str = file_get_contents('./bjyblog.sql');
+            $sql_array = preg_split("/;[\r\n]+/", str_replace('bjy_', $data['DB_PREFIX'], $bjyblog_str));
+            foreach ($sql_array as $k => $v) {
+                if (!empty($v)) {
+                    $link->query($v);
+                }
+            }
+            $link->close();
+            $db_str = <<<php
 <?php
 return array(
 
@@ -66,11 +70,13 @@ return array(
     'DB_PREFIX'             =>  '{$data['DB_PREFIX']}',   // 数据库表前缀
 );
 php;
-        // 创建数据库链接配置文件
-        file_put_contents('../../Application/Common/Conf/db.php', $db_str);
-        @touch('./install.lock');
-        require './success.html';
-    }
+            // 创建数据库链接配置文件
+            $dbfile = __DIR__ . '/../../../Application/Common/Conf/db.php';
+            file_put_contents($dbfile, $db_str);
+            @touch(SR_INSTALL_PATH . 'install.lock');
+            require SR_INSTALL_PATH . 'success.php';
+        }
+        break;
 
 }
 
--- Public/Public/install/test.php
+++ Public/Public/install/test.php
@@ -1,28 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<title>bjyblog安装程序</title>
-<script type="text/javascript" src="../static/js/jquery-2.0.0.min.js"></script>
-<link rel="stylesheet" type="text/css" href="../static/bootstrap-3.3.5/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="../static/bootstrap-3.3.5/css/bootstrap-theme.min.css">
-<link rel="stylesheet" type="text/css" href="../static/font-awesome-4.4.0/css/font-awesome.min.css">
-<link rel="stylesheet" type="text/css" href="../static/css/bjy.css">
-<script type="text/javascript" src="../static/bootstrap-3.3.5/js/bootstrap.min.js"></script>
-<!--[if lt IE 9]>
-<script type="text/javascript" src="../static/js/html5shiv.min.js"></script>
-<script type="text/javascript" src="../static/js/respond.min.js"></script>
-<![endif]-->
-<link rel="stylesheet" href="./css/install.css">
-<script>
-function testClick(){
-    if($('.yes').length!=7){
-        alert('您的配置或权限不符合要求');
-    }else{
-        location.href='./index.php?c=create';
-    }
-}
-</script>
+    <meta charset="UTF-8">
+    <title>bjyblog安装程序</title>
+    <script type="text/javascript" src="../static/js/jquery-2.0.0.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../static/bootstrap-3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../static/bootstrap-3.3.5/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="../static/font-awesome-4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../static/css/bjy.css">
+    <script type="text/javascript" src="../static/bootstrap-3.3.5/js/bootstrap.min.js"></script>
+    <!--[if lt IE 9]>
+    <script type="text/javascript" src="../static/js/html5shiv.min.js"></script>
+    <script type="text/javascript" src="../static/js/respond.min.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="./css/install.css">
+    <script>
+        function testClick() {
+            if ($('.yes').length != 7) {
+                alert('您的配置或权限不符合要求');
+            } else {
+                location.href = './index.php?c=create';
+            }
+        }
+    </script>
 </head>
 <body>
 <div id="nav">
@@ -67,9 +67,9 @@ function testClick(){
                     <td>php版本</td>
                     <td>&gt;5.3</td>
                     <td><?php echo PHP_VERSION ?></td>
-                    <?php $php_version=explode('.', PHP_VERSION); ?>
-                    <td class="<?php if(($php_version['0']>=7) || ($php_version['0']>=5 && $php_version['1']>=3))echo 'yes'; ?>">
-                        <?php if (($php_version['0']>=7) || ($php_version['0']>=5 && $php_version['1']>=3)): ?>
+                    <?php $php_version = explode('.', PHP_VERSION); ?>
+                    <td class="<?php if (($php_version['0'] >= 7) || ($php_version['0'] >= 5 && $php_version['1'] >= 3)) echo 'yes'; ?>">
+                        <?php if (($php_version['0'] >= 7) || ($php_version['0'] >= 5 && $php_version['1'] >= 3)): ?>
                             √
                         <?php else: ?>
                             ×
@@ -86,17 +86,27 @@ function testClick(){
                     <th width="25%">是否符合</th>
                 </tr>
                 <tr>
+
+                    <?php
+
+                    $current_dir = SR_INSTALL_PATH . '../../';
+                    $upload_dir = SR_INSTALL_PATH . '../../Upload/';
+                    $runtime_dir = SR_INSTALL_PATH . '../../../Runtime/';
+                    $install_dir = SR_INSTALL_PATH;
+                    $conf_dir = SR_INSTALL_PATH . '../../../Application/Common/Conf/'
+                    ?>
+
                     <td>./</td>
                     <td>可写</td>
                     <td>
-                        <?php if (is_writable('../../')): ?>
+                        <?php if (is_writable($current_dir)): ?>
                             可写
                         <?php else: ?>
                             不可写
                         <?php endif ?>
                     </td>
-                    <td class="<?php if(is_writable('../../'))echo 'yes'; ?>">
-                        <?php if (is_writable('../../')): ?>
+                    <td class="<?php if (is_writable($current_dir)) echo 'yes'; ?>">
+                        <?php if (is_writable($current_dir)): ?>
                             √
                         <?php else: ?>
                             ×
@@ -107,14 +117,14 @@ function testClick(){
                     <td>./Upload</td>
                     <td>可写</td>
                     <td>
-                        <?php if (is_writable('../../Upload')): ?>
+                        <?php if (is_writable($upload_dir)): ?>
                             可写
                         <?php else: ?>
                             不可写
                         <?php endif ?>
                     </td>
-                    <td class="<?php if(is_writable('../../Upload'))echo 'yes'; ?>">
-                        <?php if (is_writable('../../Upload')): ?>
+                    <td class="<?php if (is_writable($upload_dir)) echo 'yes'; ?>">
+                        <?php if (is_writable($upload_dir)): ?>
                             √
                         <?php else: ?>
                             ×
@@ -125,14 +135,14 @@ function testClick(){
                     <td>./Runtime</td>
                     <td>可写</td>
                     <td>
-                        <?php if (is_writable('../../Runtime')): ?>
+                        <?php if (is_writable($runtime_dir)): ?>
                             可写
                         <?php else: ?>
                             不可写
                         <?php endif ?>
                     </td>
-                    <td class="<?php if(is_writable('../../Runtime'))echo 'yes'; ?>">
-                        <?php if (is_writable('../../Runtime')): ?>
+                    <td class="<?php if (is_writable($runtime_dir)) echo 'yes'; ?>">
+                        <?php if (is_writable($runtime_dir)): ?>
                             √
                         <?php else: ?>
                             ×
@@ -143,14 +153,14 @@ function testClick(){
                     <td>./Public/install</td>
                     <td>可写</td>
                     <td>
-                        <?php if (is_writable('../../Public/install')): ?>
+                        <?php if (is_writable($install_dir)): ?>
                             可写
                         <?php else: ?>
                             不可写
                         <?php endif ?>
                     </td>
-                    <td class="<?php if(is_writable('../../Public/install'))echo 'yes'; ?>">
-                        <?php if (is_writable('../../Public/install')): ?>
+                    <td class="<?php if (is_writable($install_dir)) echo 'yes'; ?>">
+                        <?php if (is_writable($install_dir)): ?>
                             √
                         <?php else: ?>
                             ×
@@ -161,14 +171,14 @@ function testClick(){
                     <td>./Application/Common/Conf</td>
                     <td>可写</td>
                     <td>
-                        <?php if (is_writable('../../Application/Common/Conf')): ?>
+                        <?php if (is_writable($conf_dir)): ?>
                             可写
                         <?php else: ?>
                             不可写
                         <?php endif ?>
                     </td>
-                    <td class="<?php if(is_writable('../../Application/Common/Conf'))echo 'yes'; ?>">
-                        <?php if (is_writable('../../Application/Common/Conf')): ?>
+                    <td class="<?php if (is_writable($conf_dir)) echo 'yes'; ?>">
+                        <?php if (is_writable($conf_dir)): ?>
                             √
                         <?php else: ?>
                             ×
